372688c46adc935bb15855afab6f820076c2a0a1
subangkitwutama <47447319+subangkitwutama@users.noreply.github.com>
2025-06-17 15:43:54 +0700

    test title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title', 'Kementerian Kesehatan')</title>
+    <title>@yield('title', 'Kementerian')</title>
 
     @livewireStyles
 
