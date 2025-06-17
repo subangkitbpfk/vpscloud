aef3e07975b4973091cf332f689e1cab9286eae4
Subangkitbpfk <subangkit.bpfk@gmail.com>
2025-06-17 17:29:10 +0000

    server

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'Kementerian')</title>
 
+<<<<<<< HEAD
 
+=======
+   
+>>>>>>> d2aeb8d (server)
 
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -106,11 +110,19 @@
 
     <main class="py-4">
         <div class="container">
+<<<<<<< HEAD
             
         </div>
     </main>
 
 
+=======
+           
+        </div>
+    </main>
+
+ 
+>>>>>>> d2aeb8d (server)
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
